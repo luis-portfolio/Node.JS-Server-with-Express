1cbc13907409665aeeff893c34c26b12083d29f1
Luis Nt <luisnt@gmail.com>
2022-11-03 15:43:31 -0300

    upload to Subpath

--- app/controllers/File.js
+++ app/controllers/File.js
@@ -1,8 +1,8 @@
 const { mkdirSync } = require("fs")
 const { join } = require("path")
 
-const basePath = (filename) => {
-    const store = join(__dirname, '../../store/app/uploads/')
+const basePath = (filepath, filename) => {
+    const store = join(__dirname, '../../store/app/uploads/' + filepath)
     let fullPath = join(store, filename)
     // console.log({ store, fullPath });
     mkdirSync(store, { recursive: true });
@@ -12,6 +12,8 @@ const basePath = (filename) => {
 module.exports = {
     store: async (req, res) => {
         try {
+            // console.log(req);
+
             if (!req.files) {
                 res.send({
                     status: false,
@@ -21,20 +23,24 @@ module.exports = {
                 let data = [];
                 const files = Object.values(req.files)
                 files.forEach(file => {
-                    file.mv(basePath(file.name));
-                    const { name, size, mimetype } = file
-                    data.push({ file: name, bytes: size, type: mimetype });
+                    const filename = file.name
+                    const filepath = req.body[filename] || ""
+                    file.mv(basePath(filepath, file.name));
+                    const { name, size, mimetype, md5 } = file
+                    data.push({ file: name, bytes: size, type: mimetype, md5 });
                 })
 
-                // console.log(data);
-
-                //return response
-                res.send({
+                data = {
                     path: "./store/app/uploads",
                     files: data,
                     message: 'Files were saved',
                     status: true
-                });
+                }
+
+                console.log(data);
+
+                //return response
+                res.send(data)
             }
         } catch (err) {
             console.log({ err })
